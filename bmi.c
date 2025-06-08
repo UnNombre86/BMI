a7f750ca7aa73de9b768a7eba0c84ca43cbc960a
UnNombre86 <maximilianokonig2006@gmail.com>
2025-06-08 16:27:08 -0300

    Agregado estado de la persona

--- bmi.c
+++ bmi.c
@@ -4,15 +4,30 @@ int main(void)
 {
 	int height;
 	int weight;
+	float bmi;
 
 	printf("\nIngrese su altura en centímetros: ");
 	scanf("%d", &height);
 	printf("\nIngrese su peso en Kg: ");
 	scanf("%d", &weight);
 
-	printf("\nSu Índice de masa corporal es: %0.1f\n", weight/(height*height/10000.0));
+	bmi = weight/(height*height/10000.0);
 
-	printf("Índice | Condición\n<18.5 | Bajo peso\n18.5-24.9 | Peso regular\n25-29.9 | Sobrepeso\n>=30 | Obesidad\n\n\n");
+	printf("\nSu Índice de masa corporal es: %0.1f\n", bmi);
+
+	printf("Su condición es: ");
+	if(bmi < 18.5)
+		printf("Bajo peso\n");
+	else
+		if(bmi < 25)
+			printf("Peso regular\n");
+		else
+			if(bmi < 30)
+				printf("Sobrepeso\n");
+			else
+				printf("Obesidad\n");
+
+	printf("\nÍndice | Condición\n<18.5 | Bajo peso\n18.5-24.9 | Peso regular\n25-29.9 | Sobrepeso\n>=30 | Obesidad\n\n\n");
 
 	return(0);
 }
